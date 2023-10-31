a69c6cfdb57e843e0a0576de657e99d1b05a6d95
Gijs Verdonk <g.verdonk@icloud.com>
2023-10-31 22:10:45 +0100

    Add feedpulse formatter functionality.

--- FeedpulseFormatter/Program.cs
+++ FeedpulseFormatter/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.Write("Onderwerp --> ");
+var subject = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+Console.Write("Wat gaat goed? --> ");
+var going_well = Console.ReadLine();
+
+Console.Write("Wat kan beter? --> ");
+var can_be_done_better = Console.ReadLine();
+
+Console.Write("Volgende stap --> ");
+var next_steps = Console.ReadLine();
+
+string currentDateTime = DateTime.Now.ToString("dd-MM-yyyy_HH_mm_ss");
+
+string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+string fullFilePath = Path.Combine(desktopPath, $"Feedpulse_gemaakt_op_{currentDateTime}.txt");
+
+
+using (StreamWriter writer = File.CreateText(fullFilePath))
+{
+    writer.WriteLine($"Feedback over {subject}:");
+    writer.WriteLine();
+
+    writer.WriteLine($"Wat gaat volgens de docent goed?");
+    writer.WriteLine(going_well);
+    writer.WriteLine();
+
+    writer.WriteLine("Waaraan moet je volgens de docent meer aandacht besteden?");
+    writer.WriteLine(can_be_done_better);
+    writer.WriteLine();
+
+    writer.WriteLine("Wat is je volgende stap om de leeruitkomst(en) te behalen?");
+    writer.WriteLine(next_steps);
+}
+
+Console.Clear();
+Console.WriteLine($"Bestand aangemaakt. (path: {fullFilePath})");
